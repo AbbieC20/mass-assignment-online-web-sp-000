e26303b21c4e763b40950e95489147fb57501db5
remote-torrent-6747 <abbyc@sky.com>
2021-01-29 17:01:41 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,13 +2,15 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize (attributes)
+    attributes.each do |attribute|
+    end 
   end
 
 end
 
 
 
-# Each key in the attributes hash will become a 
+# Each key in the attributes hash will become a
 # property of an initialized Person instance
 
 # Your initialize method should use iteration and the .send
